4489f53ba75c2420d4d1d1ece7ea855933a1972f
tulino <tulin.ozen@bil.omu.edu.tr>
2017-07-12 14:31:31 +0300

    default article

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :articles
   has_many :comments
+  after_create :self_article
+  def self_article
+    Article.create(title:"hosgeldin", text:"hosgeldin #{self.email}", user_id: self.id)
+  end
 end
