fc1b7d8212929bb2f3632e20e948a0750a0ce68b
tulino <tulin.ozen@bil.omu.edu.tr>
2017-12-07 13:46:52 +0300

    code refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,52 +1,49 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!,except: [:index, :show]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :set_article, only: %i[show edit update destroy]
 
-def index
+  def index
     @articles = Article.all
   end
- 
-  def show
-    @article = Article.find(params[:id])
-  end
- 
+
+  def show; end
+
   def new
     @article = current_user.articles.build
   end
- 
-  def edit
-    @article = Article.find(params[:id])
-  end
- 
+
+  def edit; end
+
   def create
     @article = current_user.articles.build(article_params)
- 
+
     if @article.save
       redirect_to @article
     else
       render 'new'
     end
   end
- 
+
   def update
-    @article = Article.find(params[:id])
- 
     if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
     end
   end
- 
+
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
- 
     redirect_to articles_path
   end
- 
-  private
-    def article_params
-      params.require(:article).permit(:title, :text, :user_id)
-    end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :text, :user_id)
+  end
 end
